205fabbbe2e7ba62a1beb2546bd364405a5fc4a1
Nikhil.Kavathiya <nkavathiya@ginnovat.com>
2023-03-21 10:33:24 +0530

    updated

--- NewEvent.js
+++ NewEvent.js
@@ -1,26 +1,38 @@
 const puppeteer = require("puppeteer");
 const chromium = require("chrome-aws-lambda");
-require("dotenv").config()
+
 async function checkRunner(viewPortWidth, viewPortHeight) {
-  
-  const browser = await puppeteer.launch({
-    headless: true,
+  const exPath = await chromium.executablePath;
+  if (!exPath) {
+    return puppeteer.launch({
+      headless: true,
       visible: false,
       defaultViewport: { width: viewPortWidth, height: viewPortHeight },
+      args: [
+        "--no-sandbox",
+        "--proxy-server='direct://'",
+        "--proxy-bypass-list=*",
+      ],
+      ignoreDefaultArgs: ["--disable-extensions"],
+      ignoreHTTPSErrors: true,
+    });
+  }
+
+  return (browser = await chromium.puppeteer.launch({
     args: [
-      "--disable-setuid-sandbox",
-      "--no-sandbox",
-      "--single-process",
-      "--no-zygote",
+      ...chromium.args,
+      "--hide-scrollbars",
+      "--disable-web-security",
+      "--proxy-server='direct://'",
+      "--proxy-bypass-list=*",
     ],
-    executablePath:
-      process.env.NODE_ENV === "production"
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
-        : puppeteer.executablePath(),
-  });
-  
-  return browser;
-  
+    defaultViewport: { width: viewPortWidth, height: viewPortHeight },
+    executablePath: await chromium.executablePath,
+    headless: false,
+    visible: false,
+    ignoreDefaultArgs: ["--disable-extensions"],
+    ignoreHTTPSErrors: true,
+  }));
 }
 
 module.exports = checkRunner;
