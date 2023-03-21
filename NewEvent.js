59a79a758c55156783373f3b4c026e01a6871ad3
I am Nk <69378147+Workwithnk@users.noreply.github.com>
2023-03-21 08:55:43 +0530

    updated with docker

--- NewEvent.js
+++ NewEvent.js
@@ -2,38 +2,25 @@ const puppeteer = require("puppeteer");
 const chromium = require("chrome-aws-lambda");
 
 async function checkRunner(viewPortWidth, viewPortHeight) {
-  const exPath = await chromium.executablePath;
-  if (!exPath) {
-    return puppeteer.launch({
-      headless: true,
+  
+  const browser = await puppeteer.launch({
+    headless: true,
       visible: false,
       defaultViewport: { width: viewPortWidth, height: viewPortHeight },
-      args: [
-        "--no-sandbox",
-        "--proxy-server='direct://'",
-        "--proxy-bypass-list=*",
-        "--disable-setuid-sandbox",
-      ],
-      ignoreDefaultArgs: ["--disable-extensions"],
-      ignoreHTTPSErrors: true,
-    });
-  }
-
-  return (browser = await chromium.puppeteer.launch({
     args: [
-      ...chromium.args,
-      "--hide-scrollbars",
-      "--disable-web-security",
-      "--proxy-server='direct://'",
-      "--proxy-bypass-list=*",
+      "--disable-setuid-sandbox",
+      "--no-sandbox",
+      "--single-process",
+      "--no-zygote",
     ],
-    defaultViewport: { width: viewPortWidth, height: viewPortHeight },
-    executablePath: await chromium.executablePath,
-    headless: false,
-    visible: false,
-    ignoreDefaultArgs: ["--disable-extensions"],
-    ignoreHTTPSErrors: true,
-  }));
+    executablePath:
+      process.env.NODE_ENV === "production"
+        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        : puppeteer.executablePath(),
+  });
+  
+  return browser;
+  
 }
 
 module.exports = checkRunner;
