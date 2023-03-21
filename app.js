e6f755689e969b459f194cdb601d2b93967df38e
I am Nk <69378147+Workwithnk@users.noreply.github.com>
2023-03-21 08:10:50 +0530

    initial upload

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const app = express();
+
+const getElementDetails = require("./functions/getElemetDetails");
+
+// Wrapping the Puppeteer browser logic in a GET request
+app.get("/", async function (req, res) {
+  let customSelector = `body p`;
+  await getElementDetails(
+    "https://nikhil-kavathiya-4a985.web.app/",
+    customSelector,
+    500,
+    700
+  )
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((e) => console.log(e));
+});
+
+// Making Express listen on port 7000
+app.listen(7000, function () {
+  console.log("Running on port 7000.");
+});
