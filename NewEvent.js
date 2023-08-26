6aa1232a6b5852b14347e9735eefdb0b3548ff0a
nikhilkavathiya <nikhilkavathiya@gmail.com>
2023-08-26 18:40:54 +0530

    fggfgf

--- NewEvent.js
+++ NewEvent.js
@@ -1,38 +1,30 @@
-const puppeteer = require("puppeteer");
-const chromium = require("chrome-aws-lambda");
+const puppeteer = require('puppeteer');
+const chromium = require('chrome-aws-lambda');
 
 async function checkRunner(viewPortWidth, viewPortHeight) {
-  const exPath = await chromium.executablePath;
-  if (!exPath) {
-    return puppeteer.launch({
-      headless: true,
-      visible: false,
-      defaultViewport: { width: viewPortWidth, height: viewPortHeight },
-      args: [
-        "--no-sandbox",
-        "--proxy-server='direct://'",
-        "--proxy-bypass-list=*",
-      ],
-      ignoreDefaultArgs: ["--disable-extensions"],
-      ignoreHTTPSErrors: true,
-    });
-  }
+	const exPath = await chromium.executablePath;
+	if (!exPath) {
+		return puppeteer.launch({
+			headless: true,
+			visible: false,
+			defaultViewport: { width: viewPortWidth, height: viewPortHeight },
+			args: ['--no-sandbox', "--proxy-server='direct://'", '--proxy-bypass-list=*'],
+			ignoreDefaultArgs: ['--disable-extensions'],
+			ignoreHTTPSErrors: true
+		});
 
-  return (browser = await chromium.puppeteer.launch({
-    args: [
-      ...chromium.args,
-      "--hide-scrollbars",
-      "--disable-web-security",
-      "--proxy-server='direct://'",
-      "--proxy-bypass-list=*",
-    ],
-    defaultViewport: { width: viewPortWidth, height: viewPortHeight },
-    executablePath: await chromium.executablePath,
-    headless: false,
-    visible: false,
-    ignoreDefaultArgs: ["--disable-extensions"],
-    ignoreHTTPSErrors: true,
-  }));
+		// trthi
+	}
+
+	return (browser = await chromium.puppeteer.launch({
+		args: [...chromium.args, '--hide-scrollbars', '--disable-web-security', "--proxy-server='direct://'", '--proxy-bypass-list=*'],
+		defaultViewport: { width: viewPortWidth, height: viewPortHeight },
+		executablePath: await chromium.executablePath,
+		headless: false,
+		visible: false,
+		ignoreDefaultArgs: ['--disable-extensions'],
+		ignoreHTTPSErrors: true
+	}));
 }
 
 module.exports = checkRunner;
